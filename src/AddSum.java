7f7b73470fa055897b6c696ad63d00bcfa6ca314
qmyxhwmyqg <qmyxhwmyqg@163.com>
2025-11-17 20:05:19 +0800

    Add files via upload

---
+++ src/AddSum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class AddSum{
+    public static void main(){
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
---
+++ src/Addyuan.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Addyuan{
+    public static void main(){
+        Scanner scanner=new Scanner(System.in);
+        double r=scanner.nextInt();
+        double sum=Math.PI*r*r;
+        System.out.println(sum);
+    }
+}
